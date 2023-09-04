7c1934b2518f24519575139a19457922d0dd54b6
Praveen <96812498+162010121@users.noreply.github.com>
2023-09-04 11:43:21 +0530

    Some Changes In Update Employee Profile

--- frontend-employee/src/components/employee/EmployeeProfile.js
+++ frontend-employee/src/components/employee/EmployeeProfile.js
@@ -1,14 +1,22 @@
 import React from 'react'
+import { useState,useEffect } from 'react';
+import axios from 'axios';
+import { Link,useNavigate ,useParams} from 'react-router-dom';
 
-const EmployeeProfile = () => {
+const EditEmployee = () => {
     const { id } = useParams();
-
 	const [employee, setEmployee] = useState({
 		fristName: "",
 		lastName: "",
 		email: "",
 		department: "",
 	});
+	const {
+		fristName,
+		lastName,
+		email,
+		department,
+	} = employee;
 
 	useEffect(() => {
 		loadEmployee();
@@ -16,112 +24,114 @@ const EmployeeProfile = () => {
 
 	const loadEmployee = async () => {
 		const result = await axios.get(
-			`http://localhost:9192/students/student/${id}`
+			`http://localhost:2000/employee/getEmployee/${id}`
 		);
 		setEmployee(result.data);
 	};
 
+	
+	const handleInputChange = (e) => {
+		setEmployee({
+			...employee,
+			[e.target.name]: e.target.value,
+		});
+	};
+	
+
 	return (
-		<section
-			className="shadow"
-			style={{ backgroundColor: "whitesmoke" }}>
-			<div className="container py-5">
-				<div className="row">
-					<div className="col-lg-3">
-						<div className="card mb-4">
-							<div className="card-body text-center">
-								<img
-									src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava3.webp"
-									alt="avatar"
-									className="rounded-circle img-fluid"
-									style={{ width: 150 }}
-								/>
-								<h5 className="my-3">
-                                    {}									
-								</h5>
-								<div className="d-flex justify-content-center mb-2">
-									<button
-										type="button"
-										className="btn btn-outline-primary">
-										Call
-									</button>
-									<button
-										type="button"
-										className="btn btn-outline-warning ms-1">
-										Message
-									</button>
-								</div>
-							</div>
-						</div>
+		<div className="col-sm-8  px-5 offset-2 shadow">
+			<h2 className="mt-5">Employee Profile!</h2>
+			<form>
+				<div className="input-group mb-5">
+					<label
+						className="input-group-text"
+						htmlFor="fristName">
+						First Name
+					</label>
+					<input
+						className="form-control col-sm-6"
+						type="text"
+						name="fristName"
+						id="fristName"
+						required
+						value={fristName}
+						onChange={(e) => handleInputChange(e)}
+					/>
+				</div>
+
+				<div className="input-group mb-5">
+					<label
+						className="input-group-text"
+						htmlFor="lastName">
+						Last Name
+					</label>
+					<input
+						className="form-control col-sm-6"
+						type="text"
+						name="lastName"
+						id="lastName"
+						required
+						value={lastName}
+						onChange={(e) => handleInputChange(e)}
+					/>
+				</div>
+
+				<div className="input-group mb-5">
+					<label
+						className="input-group-text"
+						htmlFor="email">
+						Your Email
+					</label>
+					<input
+						className="form-control col-sm-6"
+						type="email"
+						name="email"
+						id="email"
+						required
+						value={email}
+						onChange={(e) => handleInputChange(e)}
+					/>
+				</div>
+
+				<div className="input-group mb-5">
+					<label
+						className="input-group-text"
+						htmlFor="department">
+						Department
+					</label>
+					<input
+						className="form-control col-sm-6"
+						type="text"
+						name="department"
+						id="department"
+						required
+						value={department}
+						onChange={(e) => handleInputChange(e)}
+					/>
+				</div>
+
+				<div className="row mb-5">
+					<div className="col-sm-2">
+						<button
+							type="submit"
+							className="btn btn-outline-success btn-lg">
+							Save
+						</button>
 					</div>
 
-					<div className="col-lg-9">
-						<div className="card mb-4">
-							<div className="card-body">
-								<hr />
-
-								<div className="row">
-									<div className="col-sm-3">
-										<h5 className="mb-0">
-											Frist Nmae
-										</h5>
-									</div>
-
-									<div className="col-sm-9">
-										<p className="text-muted mb-0">											
-										</p>
-									</div>
-								</div>
-
-								<hr />
-
-								<div className="row">
-									<div className="col-sm-3">
-										<h5 className="mb-0">
-											Last Name
-										</h5>
-									</div>
-
-									<div className="col-sm-9">
-										<p className="text-muted mb-0">											
-										</p>
-									</div>
-								</div>
-								<hr />
-
-								<div className="row">
-									<div className="col-sm-3">
-										<h5 className="mb-0">
-											Email
-										</h5>
-									</div>
-
-									<div className="col-sm-9">
-										<p className="text-muted mb-0">											
-										</p>
-									</div>
-								</div>
-								<hr />
-
-								<div className="row">
-									<div className="col-sm-3">
-										<h5 className="mb-0">
-											Department
-										</h5>
-									</div>
-
-									<div className="col-sm-9">
-										<p className="text-muted mb-0">											
-										</p>
-									</div>
-								</div>
-							</div>
-						</div>
+					<div className="col-sm-2">
+						<Link
+							to={"/view-employee"}
+							type="submit"
+							className="btn btn-outline-warning btn-lg">
+							Cancel
+						</Link>
 					</div>
 				</div>
-			</div>
-		</section>
+			</form>
+		</div>
 	);
+  
 }
 
-export default EmployeeProfile
+export default EditEmployee
